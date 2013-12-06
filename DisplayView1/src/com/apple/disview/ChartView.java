ae287870931cd5e89960c0489c8daf4a6bd433df
apple317 <hushaoping518@163.com>
2013-12-06 14:18:11 +0800

    refs
    完善柱状图形可以修改。

--- DisplayView1/src/com/apple/disview/ChartView.java
+++ DisplayView1/src/com/apple/disview/ChartView.java
@@ -20,100 +20,112 @@ import com.apple.asset.DeviceInfo;
 
 public class ChartView extends View {
 
-        private ChartEntity entity;
+	private ChartEntity entity;
 
-        
+	private Paint paint;
+	// 每个柱形宽度
+	private int zWith = 0;
+	private int zPadding = 0;
+	private int startX = 0;
+	private int totalHeight = 0;
+	private int lineHeight = 0;
+	private int zHeight = 0;
+	// 横向x坐标显示个数
+	private int wSize = 0;
+	// 纵向y坐标显示个数
+	private int hSize = 0;
 
-        private Paint paint;
-        // 每个柱形宽度
-        private int zWith = 0;
-        private int zPadding = 0;
-        private int startX = 0;
-        private int totalHeight = 0;
-        private int lineHeight = 0;
-        private int zHeight = 0;
-        // 横向x坐标显示个数
-        private int wSize = 0;
-        // 纵向y坐标显示个数
-        private int hSize = 0;
+	public ChartView(Context context, ChartEntity entity) {
+		super(context);
+		paint = new Paint();
+		paint.setAntiAlias(true);
+		this.entity = entity;
+		zWith = DeviceInfo.DipToPixels(this.getContext(), entity.row_weight);
+		zPadding = DeviceInfo.DipToPixels(this.getContext(),
+				entity.padding_weight);
+		startX = DeviceInfo.DipToPixels(this.getContext(), entity.startX);
+		zHeight = DeviceInfo.DipToPixels(this.getContext(), entity.row_height);
+		totalHeight = DeviceInfo
+				.DipToPixels(this.getContext(), entity.row_height
+						* entity.hList.size() + entity.padding_height);
+		lineHeight = DeviceInfo.DipToPixels(this.getContext(),
+				entity.row_height * entity.hList.size());
+		wSize = entity != null && entity.wList != null
+				&& entity.wList.size() > 0 ? entity.wList.size() : 0;
+		hSize = entity != null && entity.hList != null
+				&& entity.hList.size() > 0 ? entity.hList.size() : 0;
+	}
 
-        public ChartView(Context context, ChartEntity entity) {
-                super(context);
-                paint = new Paint();
-                paint.setAntiAlias(true);
-                this.entity = entity;
-                zWith = DeviceInfo.DipToPixels(this.getContext(), entity.row_weight);
-                zPadding = DeviceInfo.DipToPixels(this.getContext(),
-                                entity.padding_weight);
-                startX = DeviceInfo.DipToPixels(this.getContext(), entity.startX);
-                zHeight = DeviceInfo.DipToPixels(this.getContext(), entity.row_height);
-                totalHeight = DeviceInfo
-                                .DipToPixels(this.getContext(), entity.row_height
-                                                * entity.hList.size() + entity.padding_height);
-                lineHeight = DeviceInfo.DipToPixels(this.getContext(),
-                                entity.row_height * entity.hList.size());
-                wSize = entity != null && entity.wList != null
-                                && entity.wList.size() > 0 ? entity.wList.size() : 0;
-                hSize = entity != null && entity.hList != null
-                                && entity.hList.size() > 0 ? entity.hList.size() : 0;
-        }
+	public void drawAxis(Canvas canvas) {
+		//设置画笔属性
+		paint.setColor(Color.RED);
+		paint.setStrokeWidth(2);
+		paint.setTextSize(DeviceInfo.DipToPixels(this.getContext(), 12));
+		//用游标绘画线条
+		canvas.drawLine(startX, lineHeight, wSize * zWith + wSize * zPadding
+				+ startX, lineHeight, paint);
+		canvas.drawLine(startX, 0, startX, lineHeight, paint);
+		int x = 0;
+		int y = 0;
+		for (int i = 0; i < wSize; i++) {
+			x = startX + i * zPadding + (i) * zWith;
+			if (entity != null && entity.wList != null) {
+				String wVal = entity.wList.get(i);
+				String[] wArray = wVal.split(",");
+				if (wArray[1].equals("true")) {
+					canvas.drawText(wArray[0], x, totalHeight, paint);
+				}
+			}
+		}
+		for (int i = 0; i < hSize; i++) {
+			y = lineHeight - i * zHeight;
+			if (entity != null && entity.hList != null) {
+				String wVal = entity.hList.get(i);
+				String[] wArray = wVal.split(",");
+				if (i == 0) {
+					canvas.drawText(wArray[0],
+							DeviceInfo.DipToPixels(this.getContext(), 5), y,
+							paint);
+				} else {
+					//用游标绘画文本
+					canvas.drawText(
+							wArray[0],
+							startX
+									- DeviceInfo.DipToPixels(this.getContext(),
+											15), y, paint);
+				}
+			}
+		}
+	}
 
-        public void drawAxis(Canvas canvas) {
-                paint.setColor(Color.RED);
-                paint.setStrokeWidth(2);
-                paint.setTextSize(DeviceInfo.DipToPixels(this.getContext(), 12));
-                canvas.drawLine(startX, lineHeight, wSize * zWith + wSize * zPadding
-                                + startX + 10, lineHeight, paint);
-                canvas.drawLine(startX, 0, startX, lineHeight, paint);
-                int x = 0;
-                int y = 0;
-                for (int i = 0; i < wSize; i++) {
-                        x = startX + i * zPadding + (i) * zWith;
-                        if (entity != null && entity.wList != null) {
-                                String wVal = entity.wList.get(i);
-                                String[] wArray = wVal.split(",");
-                                if (wArray[1].equals("true")) {
-                                        canvas.drawText(wArray[0], x, totalHeight, paint);
-                                }
-                        }
-                }
-                for (int i = 0; i < hSize; i++) {
-                        y = lineHeight - i * zHeight;
-                        if (entity != null && entity.hList != null) {
-                                String wVal = entity.hList.get(i);
-                                String[] wArray = wVal.split(",");
-                                canvas.drawText(wArray[0], startX - 10, y, paint);
-                        }
-                }
-        }
+	public void drawChart(Canvas canvas) {
+		paint.setColor(Color.RED);
+		if (entity != null && entity.wList.size() > 0) {
+			for (int i = 0; i < wSize; i++) {
+				int total = (int) (entity.map.get(entity.wList.get(i)) == null ? 0
+						: entity.map.get(entity.wList.get(i)) * entity.scale);
+				//用游标画一个方块
+				canvas.drawRect((i + 1) * zPadding + (i) * zWith + startX,
+						lineHeight - total, i * zPadding + i * zWith + startX,
+						lineHeight, paint);
+			}
+		}
+	}
 
-        public void drawChart(Canvas canvas) {
-                paint.setColor(Color.RED);
-                if (entity != null && entity.wList.size() > 0) {
-                        for (int i = 0; i < wSize; i++) {
-                                int total = (int) (entity.map.get(entity.wList.get(i)) == null ? 0
-                                                : entity.map.get(entity.wList.get(i)) * entity.scale);
-                                canvas.drawRect((i+1) * zPadding + (i) * zWith + startX,
-                                                lineHeight - total, i * zPadding + i * zWith + startX,
-                                                lineHeight, paint);
-                        }
-                }
-        }
+	@Override
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		final int heightSize = MeasureSpec.getSize(totalHeight);
+		setMeasuredDimension(widthSize, heightSize);
+	}
 
-        @Override
-        public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-                super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-                final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-                final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-                final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-                final int heightSize = MeasureSpec.getSize(totalHeight);
-                setMeasuredDimension(widthSize, heightSize);
-        }
-
-        @Override
-        public void onDraw(Canvas canvas) {
-                drawAxis(canvas);
-                drawChart(canvas);
-        }
+	@Override
+	public void onDraw(Canvas canvas) {
+		drawAxis(canvas);
+		drawChart(canvas);
+	}
 
 }
--- DisplayView1/src/com/apple/disview/activity/ChartActivity.java
+++ DisplayView1/src/com/apple/disview/activity/ChartActivity.java
@@ -1,6 +1,5 @@
 package com.apple.disview.activity;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,72 +14,82 @@ import com.apple.asset.ChartEntity;
 import com.apple.disview.R;
 import com.apple.disview.ChartView;
 
-public class ChartActivity extends Activity{
+/**
+ * 柱状形显示demo
+ * 
+ * @author Administrator
+ * 
+ */
+public class ChartActivity extends Activity {
 
-        private LinearLayout layout_chart;
-        
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                // TODO Auto-generated method stub
-                super.onCreate(savedInstanceState);
-                this.setContentView(R.layout.layout_chart);
-                layout_chart=(LinearLayout) this.findViewById(R.id.lay_chart);
-                LinearLayout.LayoutParams layParam = new LinearLayout.LayoutParams(
-                                LayoutParams.WRAP_CONTENT,
-                                LayoutParams.WRAP_CONTENT);
-                layParam.gravity = Gravity.CENTER_HORIZONTAL;
-            
-                ChartEntity entity=new ChartEntity();
-                entity.scale=6;
-                entity.row_height=60;
-                entity.row_weight=4;
-                entity.padding_weight=8;
-                List<String> hList=new ArrayList<String>();
-                List<String> wList=new ArrayList<String>();
-                HashMap<String,Integer> map=new HashMap<String,Integer>();
-                hList.add("0鹿芦脌茂");
-                hList.add("10");
-                hList.add("20");
-                hList.add("30");
-                entity.hList=hList;
-                for(int i=0;i<30;i++){
-                        String curStr;
-                        if((i+1)%5==0){
-                                curStr=(i+1)+",true";
-                        }else{
-                                curStr=(i+1)+",false";
-                        }
-                        wList.add(curStr);
-                        map.put(curStr, i*4);
-                }
-                entity.wList=wList;
-                entity.map=map;
-                ChartView chartView = new ChartView(this.getApplicationContext(), entity);
-                layout_chart.addView(chartView, layParam);
-        }
+	private LinearLayout layout_chart;
 
-        @Override
-        protected void onDestroy() {
-                // TODO Auto-generated method stub
-                super.onDestroy();
-        }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		// 先编写一个布局文件
+		this.setContentView(R.layout.layout_chart);
+		//创建一个线性容器，之后就是向着里面嵌套视图
+		layout_chart = (LinearLayout) this.findViewById(R.id.lay_chart);
+		LinearLayout.LayoutParams layParam = new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		layParam.gravity = Gravity.CENTER_HORIZONTAL;
+		//创建柱状性参数对象
+		ChartEntity entity = new ChartEntity();
+		entity.scale = 6;
+		entity.row_height = 60;
+		entity.row_weight = 4;
+		entity.padding_weight = 8;
+		//创建一个纵向数据容器
+		List<String> hList = new ArrayList<String>();
+		//创建一个横向数据容器，注解数据格式如:key,是否显示在横坐标
+		List<String> wList = new ArrayList<String>();
+		//创建一个存储高度值容器
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		hList.add("0公里");
+		hList.add("10-");
+		hList.add("20-");
+		hList.add("30-");
+		entity.hList = hList;
+		for (int i = 0; i < 30; i++) {
+			String curStr;
+			if ((i + 1) % 5 == 0) {
+				curStr = (i + 1) + ",true";
+			} else {
+				curStr = (i + 1) + ",false";
+			}
+			wList.add(curStr);
+			map.put(curStr, i * 4);
+		}
+		entity.wList = wList;
+		entity.map = map;
+		ChartView chartView = new ChartView(this.getApplicationContext(),
+				entity);
+		layout_chart.addView(chartView, layParam);
+	}
 
-        @Override
-        protected void onPause() {
-                // TODO Auto-generated method stub
-                super.onPause();
-        }
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
 
-        @Override
-        protected void onResume() {
-                // TODO Auto-generated method stub
-                super.onResume();
-        }
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
 
-        @Override
-        protected void onStart() {
-                // TODO Auto-generated method stub
-                super.onStart();
-        }
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
 
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
 }
